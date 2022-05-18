9a39755d80ed60a14ac55785f9dee5da3c298630
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-05-17 22:04:35 -0700

    File Structure

---
+++ public/js/login.js
@@ -0,0 +1,66 @@
+console.log("login linked1")
+document.querySelector("#login").addEventListener("submit",e=>{
+    e.preventDefault();
+    const userObj = {
+        username:document.querySelector("#loginUsername").value,
+        password:document.querySelector("#loginPassword").value,
+    }
+    console.log(userObj)
+    fetch("/api/users/login",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            location.href="/profile"
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
+
+document.querySelector("#signup").addEventListener("submit",e=>{
+    e.preventDefault();
+    const userObj = {
+        username:document.querySelector("#signupUsername").value,
+        password:document.querySelector("#signupPassword").value,
+    }
+    console.log(userObj)
+    fetch("/api/users/",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            location.href="/profile"
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
+console.log("login linked1")
+document.querySelector("#signup").addEventListener("submit",e=>{
+    e.preventDefault();
+    const userObj = {
+        username:document.querySelector("#signupUsername").value,
+        password:document.querySelector("#signupPassword").value,
+    }
+    console.log(userObj)
+    fetch("/api/users/login",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            console.log("logged in!")
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
